7b51f0bc9c2e6dda6b2a74c3c4e39097aa55610d
Limon <sorkerlimon18@gmail.com>
2025-01-03 09:51:09 +0600

    first commit

---
+++ __main__.py
@@ -0,0 +1,29 @@
+
+import pulumi
+import pulumi_aws as aws
+
+# Reference existing VPC
+vpc = aws.ec2.Vpc.get("classtune-vpc",
+    id="vpc-064681d4b6b8fa26c"
+)
+
+# Reference existing subnet
+subnet = aws.ec2.Subnet.get("classtune-subnet-public1-ap-southeast-1a",
+    id="subnet-0f44ca2c89fe0990d"
+)
+
+# EC2 Instance
+instance = aws.ec2.Instance("classtune-ec2",
+    instance_type="t2.micro",
+    ami="ami-06650ca7ed78ff6fa",  # Ubuntu 20.04 LTS in ap-southeast-1
+    subnet_id=subnet.id,
+    vpc_security_group_ids=["sg-0f3718488c1032dc5"],
+    key_name="classtune-limon",
+    tags={
+        "Name": "classtune-ec2"
+    }
+)
+
+# Export the instance's public IP and public DNS
+pulumi.export('instance_public_ip', instance.public_ip)
+pulumi.export('instance_public_dns', instance.public_dns)
